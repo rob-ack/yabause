2e284e5c87cc437cf4e01923406d630f19871f63
Robert Acksel <robert.acksel@googlemail.com>
2021-07-31 17:32:48 +0200

    -fix: retropie so file name was wrong

--- Retropie/installscript/lr-kronos/lr-kronos-rob-ack.sh
+++ Retropie/installscript/lr-kronos/lr-kronos-rob-ack.sh
@@ -13,7 +13,7 @@ rp_module_id="lr-kronos-rob-ack"
 rp_module_desc="Sega Saturn emu - Kronos Yabause (optimised) port for libretro"
 rp_module_help="ROM Extensions: .iso .bin .zip\n\nCopy your Sega Saturn roms to $romdir/saturn\n\nCopy the required BIOS file saturn_bios.bin to $biosdir"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/yabause/master/yabause/COPYING"
-rp_module_repo="git https://github.com/rob-ack/yabause.git kronos-cmake_ci"
+rp_module_repo="git https://github.com/rob-ack/yabause.git kronos-retroarch-retropie-integration"
 rp_module_section="exp"
 rp_module_flags="!armv6"
 
@@ -27,12 +27,12 @@ function build_lr-kronos-rob-ack() {
     rm -f ./out/CMakeCache.txt
     cmake -DKRONOS_LIBRETRO_CORE:BOOL=True -DOPENGLCORE_TEST:BOOL=False -DYAB_WANT_ARM7:BOOL=True -DYAB_USE_QT5:BOOL=False -DYAB_PORTS:STRING="" -DYAB_WANT_SH2_ASYNC:BOOL=True -S . -B ./out
     cmake --build ./out --config Release -- -j4
-    md_ret_require="$md_build/yabause/out/src/libretro/kronos_libretro.so"
+    md_ret_require="$md_build/yabause/out/src/libretro/libkronos_libretro.so"
 }
 
 function install_lr-kronos-rob-ack() {
     md_ret_files=(
-        'yabause/out/src/libretro/kronos_libretro.so'
+        'yabause/out/src/libretro/libkronos_libretro.so'
         'yabause/COPYING'
         'yabause/ChangeLog'
         'yabause/AUTHORS'
@@ -44,6 +44,6 @@ function configure_lr-kronos-rob-ack() {
     mkRomDir "saturn"
     ensureSystemretroconfig "saturn"
 
-    addEmulator 1 "$md_id" "saturn" "$md_inst/kronos_libretro.so"
+    addEmulator 1 "$md_id" "saturn" "$md_inst/libkronos_libretro.so"
     addSystem "saturn"
 }

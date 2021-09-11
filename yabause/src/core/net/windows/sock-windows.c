46d02f5b6476c61f989be67b16cc9b907813ed6c
Robert Acksel <robert.acksel@googlemail.com>
2021-09-11 16:00:43 +0200

    fix: -include path file names are not respecting case sensitivity

--- yabause/src/core/net/windows/sock-windows.c
+++ yabause/src/core/net/windows/sock-windows.c
@@ -30,8 +30,8 @@
 #define _WIN32_WINNT 0x501
 #endif
 
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
 static fd_set read_fds;
 static fd_set write_fds;

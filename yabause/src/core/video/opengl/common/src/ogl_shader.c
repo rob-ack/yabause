fb07c426e694d184fac46f5448377e7bba7a76a6
Robert Acksel <robert.acksel@googlemail.com>
2021-09-17 19:31:56 +0200

    fix: -warning

--- yabause/src/core/video/opengl/common/src/ogl_shader.c
+++ yabause/src/core/video/opengl/common/src/ogl_shader.c
@@ -1050,7 +1050,7 @@ vec2 getFBCoord(vec2 pos) { return pos; } \n \
 
 
 #define SAMPLER_TEX(ID) "\
-uniform sampler2D s_texture"Stringify(ID)";\n \
+uniform sampler2D s_texture" Stringify(ID) ";\n \
 "
 //--------------------------------------------------------------------------------------------------------------
 static const char vdp2blit_gl_start_f_6[] =

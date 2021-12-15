38bdda9a9479b49e5487b72178631501f964cb5f
Francois CARON <francois.caron@renault.com>
2021-12-15 14:48:28 +0100

    Fix debug info for windows

--- yabause/src/sys/vdp2/src/vdp2debug.c
+++ yabause/src/sys/vdp2/src/vdp2debug.c
@@ -176,7 +176,7 @@ static INLINE char *AddWindowInfoString(char *outstring, int wctl, int issprite)
          AddString(outstring, "Vertical end = %d\r\n", vend);
       }
 
-      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x1) ? "inside" : "outside");
+      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x1) ? "outside" : "inside");
    }
 
    if (wctl & 0x8)
@@ -201,13 +201,13 @@ static INLINE char *AddWindowInfoString(char *outstring, int wctl, int issprite)
          AddString(outstring, "Vertical end = %d\r\n", vend);
       }
 
-      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x4) ? "inside" : "outside");
+      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x4) ? "outside" : "inside");
    }
 
    if (wctl & 0x20)
    {
       AddString(outstring, "Using Sprite Window:\r\n");
-      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x10) ? "inside" : "outside");
+      AddString(outstring, "Display %s of Window\r\n", (wctl & 0x10) ? "outside" : "inside");
    }
 
    if (wctl & 0x2A)
